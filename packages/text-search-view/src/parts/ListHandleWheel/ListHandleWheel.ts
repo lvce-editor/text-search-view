89d8a6761a70218cd8216c5f9daa73a76757c644
Le Vivilet <levivilet@pm.me>
2026-07-24 16:29:16 +0000

    fix: satisfy state destructuring lint

--- packages/text-search-view/src/parts/ListHandleWheel/ListHandleWheel.ts
+++ packages/text-search-view/src/parts/ListHandleWheel/ListHandleWheel.ts
@@ -3,8 +3,9 @@ import type { SearchState } from '../SearchState/SearchState.ts'
 import * as GetFileIcons from '../GetFileIcons/GetFileIcons.ts'
 
 export const handleWheel = async (state: SearchState, deltaMode: number, deltaY: number): Promise<SearchState> => {
+  const { maxLineY: oldMaxLineY, minLineY: oldMinLineY } = state
   const newState = handleListWheel(state, deltaMode, deltaY)
-  if (newState.minLineY === state.minLineY && newState.maxLineY === state.maxLineY) {
+  if (newState.minLineY === oldMinLineY && newState.maxLineY === oldMaxLineY) {
     return newState
   }
   const { fileIconCache, listItems, maxLineY, minLineY } = newState
