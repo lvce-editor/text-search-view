22671b662a86316d0d20f04813cca5d216e3f616
Le Vivilet <levivilet@pm.me>
2026-08-02 14:29:09 -0700

    fix: request open editor uris for filtered searches (#18)

--- packages/text-search-view/src/parts/TextSearch/TextSearch.ts
+++ packages/text-search-view/src/parts/TextSearch/TextSearch.ts
@@ -1,6 +1,7 @@
-import { TextSearchWorker } from '@lvce-editor/rpc-registry'
+import { RendererWorker, TextSearchWorker } from '@lvce-editor/rpc-registry'
 import type { TextSearchCompletionResult } from '../TextSearchCompletionResult/TextSearchCompletionResult.ts'
 import type { TextSearchOptions } from '../TextSearchOptions/TextSearchOptions.ts'
+import * as SearchFlags from '../SearchFlags/SearchFlags.ts'
 
 export const textSearch = async (
   root: string,
@@ -11,5 +12,9 @@ export const textSearch = async (
   searchId?: string,
   uid?: number,
 ): Promise<TextSearchCompletionResult> => {
-  return TextSearchWorker.search(root, query, options, assetDir, platform, searchId, uid)
+  if (!SearchFlags.hasOpenEditors(options.flags)) {
+    return TextSearchWorker.search(root, query, options, assetDir, platform, searchId, uid)
+  }
+  const openEditorUris = await RendererWorker.invoke('GetActiveEditor.getOpenEditorUris')
+  return TextSearchWorker.search(root, query, { ...options, openEditorUris }, assetDir, platform, searchId, uid)
 }
