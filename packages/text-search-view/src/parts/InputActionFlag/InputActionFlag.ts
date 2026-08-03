45dc6aa7931a76aea3877761440c2f0ee20141cc
Le Vivilet <levivilet@pm.me>
2026-08-02 18:12:12 -0700

    fix: expose replace all as a button (#20)

--- packages/text-search-view/src/parts/InputActionFlag/InputActionFlag.ts
+++ packages/text-search-view/src/parts/InputActionFlag/InputActionFlag.ts
@@ -1,4 +1,4 @@
 export const CheckBoxEnabled = 1
 export const CheckBoxDisabled = 2
-export const ButtonEnabled = 2
-export const ButtonDisabled = 3
+export const ButtonEnabled = 3
+export const ButtonDisabled = 4
