0d49e8f19c5887d02d909c47aae4031d725af0f4
Le Vivilet <levivilet@pm.me>
2026-08-15 08:01:02 +0000

    feature: render text search commands directly

--- packages/text-search-view/src/parts/Render2/Render2.ts
+++ packages/text-search-view/src/parts/Render2/Render2.ts
@@ -1,9 +1,21 @@
+import { ViewletCommand } from '@lvce-editor/constants'
 import * as ApplyRender from '../ApplyRender/ApplyRender.ts'
+import * as RendererProcess from '../RendererProcess/RendererProcess.ts'
 import * as SearchViewStates from '../SearchViewStates/SearchViewStates.ts'
 
-export const render2 = (uid: number, diffResult: readonly number[]): readonly any[] => {
+const renderDirect = async (uid: number, commands: readonly any[]): Promise<readonly any[]> => {
+  const rendererWorkerCommands = commands.filter((command) => command[0] === ViewletCommand.SetFocusContext)
+  const rendererProcessCommands = commands.filter((command) => command[0] !== ViewletCommand.SetFocusContext)
+  const transactionId = await RendererProcess.invoke('Viewlet.queueCommands', uid, rendererProcessCommands)
+  return [...rendererWorkerCommands, ['Viewlet.commitPending', uid, transactionId]]
+}
+
+export const render2 = (uid: number, diffResult: readonly number[]): readonly any[] | Promise<readonly any[]> => {
   const { newState, oldState } = SearchViewStates.get(uid)
   SearchViewStates.set(uid, newState, newState)
   const commands = ApplyRender.applyRender(oldState, newState, diffResult)
-  return commands
+  if (!RendererProcess.isConnected()) {
+    return commands
+  }
+  return renderDirect(uid, commands)
 }
