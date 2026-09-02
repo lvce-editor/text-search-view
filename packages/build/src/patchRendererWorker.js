e38270d6cfb04acf777b35d4b82a95019e345ea8
Le Vivilet <levivilet@pm.me>
2026-09-02 21:16:36 +0000

    test: update renderer worker compatibility patch

--- packages/build/src/patchRendererWorker.js
+++ packages/build/src/patchRendererWorker.js
@@ -3,6 +3,8 @@ export const patchRendererWorker = (content, remoteUrl = '', useRemoteUrl = true
 
   newContent = newContent.replaceAll('http://localhost:3001/tests/', '/tests/')
 
+  newContent = newContent.replace(`  await invoke$H(ipc, 'TextSearch.initialize', getPlatform$2());\n`, '')
+
   if (useRemoteUrl && remoteUrl && !newContent.includes('// const textSearchWorkerUrl = ')) {
     const occurrence = `const textSearchWorkerUrl = \`\${assetDir}/packages/text-search-view/dist/textSearchViewMain.js\``
     const replacement = `// const textSearchWorkerUrl = \`\${assetDir}/packages/text-search-view/dist/textSearchViewMain.js\`
