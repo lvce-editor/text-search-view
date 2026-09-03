dd98794a2527971cd5ca4610aa8015b375a872ad
Le Vivilet <levivilet@pm.me>
2026-09-03 20:31:06 +0000

    fix: add missing text search setting

--- packages/text-search-view/test/Settings.test.ts
+++ packages/text-search-view/test/Settings.test.ts
@@ -1,10 +1,24 @@
 import { expect, test } from '@jest/globals'
 import { readFile } from 'node:fs/promises'
 
-test('search.exclude has an array default', async () => {
+interface Setting {
+  readonly id?: string
+}
+
+const loadSettings = async (): Promise<readonly Setting[]> => {
   const content = await readFile(new URL('../settings.json', import.meta.url), 'utf8')
-  const settings = JSON.parse(content)
-  const searchExclude = settings.find((setting: { readonly id?: string }) => setting.id === 'search.exclude')
+  return JSON.parse(content) as readonly Setting[]
+}
+
+test('contains all text search settings', async () => {
+  const settings = await loadSettings()
+
+  expect(settings.map((setting) => setting.id)).toEqual(['search.exclude', 'search.threads', 'Search.usePullBasedSearch'])
+})
+
+test('search.exclude has an array default', async () => {
+  const settings = await loadSettings()
+  const searchExclude = settings.find((setting) => setting.id === 'search.exclude')
 
   expect(searchExclude).toEqual(
     expect.objectContaining({
