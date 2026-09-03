3c11d83cd2186f076414bafd57e2099b622aa2c5
Le Vivilet <levivilet@pm.me>
2026-09-03 14:01:01 +0000

    refactor: reuse dialog worker prompt

--- packages/text-search-view/src/parts/ConfirmPrompt/ConfirmPrompt.ts
+++ packages/text-search-view/src/parts/ConfirmPrompt/ConfirmPrompt.ts
@@ -1,5 +1,3 @@
 import { DialogWorker } from '@lvce-editor/rpc-registry'
 
-export const prompt = async (text: string, options: DialogWorker.ConfirmPromptOptions): Promise<boolean> => {
-  return DialogWorker.prompt(text, options)
-}
+export const prompt = DialogWorker.prompt
