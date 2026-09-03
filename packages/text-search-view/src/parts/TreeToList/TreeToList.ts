dd18a350544e66359c4c6bde39c719506de0595e
Le Vivilet <levivilet@pm.me>
2026-09-03 13:50:42 +0000

    Hoist tree child processing (#56)

--- packages/text-search-view/src/parts/TreeToList/TreeToList.ts
+++ packages/text-search-view/src/parts/TreeToList/TreeToList.ts
@@ -3,29 +3,31 @@ import type { Tree } from '../Tree/Tree.ts'
 import { join2 } from '../Path/Path.ts'
 import * as TextSearchResultType from '../TextSearchResultType/TextSearchResultType.ts'
 
-export const treeToList = (map: Tree, root: string): readonly SearchResult[] => {
-  const items: SearchResult[] = []
-  const processChildren = (path: string, depth: number): void => {
-    const children = map[path]
-    if (!children) {
-      return
-    }
-    const count = children.length
-    for (let i = 0; i < count; i++) {
-      const child = children[i]
-      if (child.type === TextSearchResultType.File) {
-        const childPath = join2(path, child.text)
-        const absolutePath = `${root}${childPath}`
-        items.push({
-          ...child,
-          text: absolutePath,
-        })
-        processChildren(childPath, depth + 1)
-      } else {
-        items.push(child)
-      }
+// eslint-disable-next-line @typescript-eslint/prefer-readonly-parameter-types
+const processChildren = (map: Tree, items: SearchResult[], root: string, path: string, depth: number): void => {
+  const children = map[path]
+  if (!children) {
+    return
+  }
+  const count = children.length
+  for (let i = 0; i < count; i++) {
+    const child = children[i]
+    if (child.type === TextSearchResultType.File) {
+      const childPath = join2(path, child.text)
+      const absolutePath = `${root}${childPath}`
+      items.push({
+        ...child,
+        text: absolutePath,
+      })
+      processChildren(map, items, root, childPath, depth + 1)
+    } else {
+      items.push(child)
     }
   }
-  processChildren('', 0)
+}
+
+export const treeToList = (map: Tree, root: string): readonly SearchResult[] => {
+  const items: SearchResult[] = []
+  processChildren(map, items, root, '', 0)
   return items
 }
