06de240ce1428c429dbfc6a0d1b106ca2334364a
Le Vivilet <levivilet@pm.me>
2026-09-03 13:51:05 +0000

    Extract search history update (#57)

---
+++ packages/text-search-view/test/GetNewHistory.test.ts
@@ -0,0 +1,11 @@
+import { expect, test } from '@jest/globals'
+import * as GetNewHistory from '../src/parts/GetNewHistory/GetNewHistory.ts'
+
+test('appends the new value', () => {
+  expect(GetNewHistory.getNewHistory(['first'], 'second')).toEqual(['first', 'second'])
+})
+
+test('limits the history to 100 entries', () => {
+  const history = Array.from({ length: 100 }, (_, index) => String(index))
+  expect(GetNewHistory.getNewHistory(history, 'new value')).toEqual(history)
+})
