69aa6f58c4c75d46d3a89a1e1774cadda15ae0a9
Le Vivilet <levivilet@pm.me>
2026-09-03 12:33:54 +0000

    fix: order action button imports

--- packages/text-search-view/src/parts/GetActionButtonVirtualDom/GetActionButtonVirtualDom.ts
+++ packages/text-search-view/src/parts/GetActionButtonVirtualDom/GetActionButtonVirtualDom.ts
@@ -1,8 +1,8 @@
 import { VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
 import type { Action } from '../Action/Action.ts'
+import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 import * as GetActionButtonClassName from '../GetActionButtonClassName/GetActionButtonClassName.ts'
 import * as GetIconVirtualDom from '../GetIconVirtualDom/GetIconVirtualDom.ts'
-import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 
 export const getActionButtonVirtualDom = (action: Action): readonly VirtualDomNode[] => {
   const { enabled, icon, id, label } = action
