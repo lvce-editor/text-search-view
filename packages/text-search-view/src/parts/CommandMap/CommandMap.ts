c35f24537fed71df41fd9a13b398f1f3da166c71
Le Vivilet <levivilet@pm.me>
2026-09-05 07:42:26 +0000

    feature: expose component virtual DOM for inspection (#68)

--- packages/text-search-view/src/parts/CommandMap/CommandMap.ts
+++ packages/text-search-view/src/parts/CommandMap/CommandMap.ts
@@ -19,6 +19,7 @@ import * as FocusPreviousInput from '../FocusPreviousInput/FocusPreviousInput.ts
 import * as FocusReplaceValue from '../FocusReplaceValue/FocusReplaceValue.ts'
 import * as FocusSearchValue from '../FocusSearchValue/FocusSearchValue.ts'
 import * as FocusSearchValueNext from '../FocusSearchValueNext/FocusSearchValueNext.ts'
+import { getComponentDom } from '../GetComponentDom/GetComponentDom.ts'
 import * as GetComponentState from '../GetComponentState/GetComponentState.ts'
 import * as GetKeyBindings from '../GetKeyBindings/GetKeyBindings.ts'
 import { getMenuEntries } from '../GetMenuEntries/GetMenuEntries.ts'
@@ -140,6 +141,7 @@ export const commandMap = {
   'TextSearch.focusSearchValue': WrapCommand.wrapCommand(FocusSearchValue.focusSearchValue),
   'TextSearch.focusSearchValueNext': WrapCommand.wrapCommand(FocusSearchValueNext.focusSearchValueNext),
   'TextSearch.getCommandIds': WrapCommand.getCommandIds,
+  'TextSearch.getComponentDom': getComponentDom,
   'TextSearch.getComponentState': GetComponentState.getComponentState,
   'TextSearch.getKeyBindings': GetKeyBindings.getKeyBindings,
   'TextSearch.getMenuEntries': WrapCommand.wrapGetter(getMenuEntries),
