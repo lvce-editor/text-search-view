a2effe78180a504e48c883292f5da23a5859d6e0
Le Vivilet <levivilet@pm.me>
2026-09-14 13:25:22 +0200

    fix: rerun search when context lines change (#74)

--- packages/text-search-view/src/parts/ToggleContextLines/ToggleContextLines.ts
+++ packages/text-search-view/src/parts/ToggleContextLines/ToggleContextLines.ts
@@ -1,9 +1,7 @@
 import type { SearchState } from '../SearchState/SearchState.ts'
+import * as HandleUpdate from '../HandleUpdate/HandleUpdate.ts'
 
 export const toggleContextLines = async (state: SearchState): Promise<SearchState> => {
   const { contextLinesEnabled } = state
-  return {
-    ...state,
-    contextLinesEnabled: !contextLinesEnabled,
-  }
+  return HandleUpdate.handleUpdate(state, { contextLinesEnabled: !contextLinesEnabled })
 }
--- packages/text-search-view/test/ToggleContextLines.test.ts
+++ packages/text-search-view/test/ToggleContextLines.test.ts
@@ -1,4 +1,5 @@
 import { expect, test } from '@jest/globals'
+import { TextMeasurementWorker, TextSearchWorker } from '@lvce-editor/rpc-registry'
 import * as CreateDefaultState from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
 import { toggleContextLines } from '../src/parts/ToggleContextLines/ToggleContextLines.ts'
 
@@ -6,4 +7,28 @@ test('toggleContextLines', async () => {
   const state = CreateDefaultState.createDefaultState()
   const newState = await toggleContextLines(state)
   expect(newState.contextLinesEnabled).toBe(true)
+})
+
+test('toggleContextLines - reruns the search with context enabled', async () => {
+  using _mockTextMeasurementWorker = TextMeasurementWorker.registerMockRpc({
+    'TextMeasurement.measureTextBlockHeight': () => 13,
+  })
+  let receivedOptions: any
+  using _mockTextSearchWorker = TextSearchWorker.registerMockRpc({
+    async 'TextSearch.search'(_root: string, _query: string, options: any) {
+      receivedOptions = options
+      return { limitHit: false, results: [] }
+    },
+  })
+  const state = {
+    ...CreateDefaultState.createDefaultState(),
+    contextLines: 2,
+    uid: 202,
+    value: 'needle',
+    workspaceUri: 'file:///test',
+  }
+
+  await toggleContextLines(state)
+
+  expect(receivedOptions.contextLines).toBe(2)
 })
