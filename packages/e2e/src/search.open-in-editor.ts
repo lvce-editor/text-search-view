cb94a6247b8e89c18dbf155af63b9b5f0824f545
Le Vivilet <levivilet@pm.me>
2026-09-23 06:49:26 -0700

    test: verify open search editor results (#83)

--- packages/e2e/src/search.open-in-editor.ts
+++ packages/e2e/src/search.open-in-editor.ts
@@ -11,9 +11,22 @@ export const test: Test = async ({ expect, FileSystem, Locator, Search, SideBar,
   await Search.setValue('needle')
 
   const sideBarSearch = Locator('.Search')
+  const sideBarTreeItems = sideBarSearch.locator('[role="treeitem"]')
+  await expect(sideBarTreeItems).toHaveCount(2)
   const sideBarMessage = sideBarSearch.locator('[role="status"]')
   await expect(sideBarMessage).toHaveText('1 result in 1 file')
   const openInEditor = sideBarSearch.locator('button[name="OpenSearchEditor"]')
   await expect(openInEditor).toHaveAttribute('title', 'Copy current search results to an editor (Alt+Enter)')
   await openInEditor.click()
+
+  const searches = Locator('.Search')
+  await expect(searches).toHaveCount(2)
+  const editorSearch = searches.nth(1)
+  const editorMessage = editorSearch.locator('[role="status"]')
+  const editorSearchValue = editorSearch.locator('textarea[name="SearchValue"]')
+  const editorTreeItems = editorSearch.locator('[role="treeitem"]')
+  await expect(editorSearch).toBeVisible()
+  await expect(editorSearchValue).toHaveValue('needle')
+  await expect(editorMessage).toHaveText('1 result in 1 file')
+  await expect(editorTreeItems).toHaveCount(2)
 }
